3895bcdcb111f06e44a629096e3d6810a193154e
kristofanf <kristofan.feriadi@ti.ukdw.ac.id>
2023-07-24 08:50:36 +0700

    update deploy

--- deploy.sh
+++ deploy.sh
@@ -17,6 +17,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:kuzmicni/paviliun900.git main:gh-pages
+git push -f git@github.com:kristofanf/paviliun900.git main:gh-pages
 
 cd -
